83a06faaf0e66f4aa923cd33739ce8eff591c9fc
master <pacheco.anasof@gmail.com>
2023-04-25 22:16:06 -0500

    inicia el proyecto

---
+++ script.js
@@ -0,0 +1,98 @@
+const banner = {
+    imagen: "https://assets-metrostyle.abs-cbn.com/prod/metro.style/media/2021/may/kdrama-hometown-cha-cha-cha-netflix-tvn-desktop.jpg?ext=.jpg",
+    titulo: "Hometown Cha Cha Cha",
+    descripcion: "Yoon Hye-jin, an accomplished dentist from Seoul, goes to the idyllic seaside village of Gongjin on her late mother's birthday after her life goes awry. She spontaneously decides to open up a dental clinic, and gets to know jack-of-all-trades Hong Du-sik, also known as Chief Hong...",
+    calificacion: "4.8",
+    clasificacion: "13+",
+};
+
+const recomendaciones = [
+    
+    {imagen: "https://assets-metrostyle.abs-cbn.com/prod/metro.style/media/2021/may/kdrama-hometown-cha-cha-cha-netflix-tvn-desktop.jpg?ext=.jpg",
+    titulo: "Hometown Cha Cha Cha",
+    descripcion: "Yoon Hye-jin, an accomplished dentist from Seoul, goes to the idyllic seaside village of Gongjin on her late mother's birthday after her life goes awry. She spontaneously decides to open up a dental clinic, and gets to know jack-of-all-trades Hong Du-sik, also known as Chief Hong...",
+    calificacion: "4.8",
+    clasificacion: "13+",
+},
+     {
+    imagen: "https://assets-metrostyle.abs-cbn.com/prod/metro.style/media/2021/may/kdrama-hometown-cha-cha-cha-netflix-tvn-desktop.jpg?ext=.jpg",
+    titulo: "Prueba",
+    descripcion: "Yoon Hye-jin, an accomplished dentist from Seoul, goes to the idyllic seaside village of Gongjin on her late mother's birthday after her life goes awry. She spontaneously decides to open up a dental clinic, and gets to know jack-of-all-trades Hong Du-sik, also known as Chief Hong...",
+    calificacion: "4.8",
+    clasificacion: "13+",
+},
+     {
+    imagen: "https://assets-metrostyle.abs-cbn.com/prod/metro.style/media/2021/may/kdrama-hometown-cha-cha-cha-netflix-tvn-desktop.jpg?ext=.jpg",
+    titulo: "Vincenzo",
+    descripcion: "Yoon Hye-jin, an accomplished dentist from Seoul, goes to the idyllic seaside village of Gongjin on her late mother's birthday after her life goes awry. She spontaneously decides to open up a dental clinic, and gets to know jack-of-all-trades Hong Du-sik, also known as Chief Hong...",
+    calificacion: "4.8",
+    clasificacion: "13+",
+},
+     {
+    imagen: "https://assets-metrostyle.abs-cbn.com/prod/metro.style/media/2021/may/kdrama-hometown-cha-cha-cha-netflix-tvn-desktop.jpg?ext=.jpg",
+    titulo: "One day at the time",
+    descripcion: "Yoon Hye-jin, an accomplished dentist from Seoul, goes to the idyllic seaside village of Gongjin on her late mother's birthday after her life goes awry. She spontaneously decides to open up a dental clinic, and gets to know jack-of-all-trades Hong Du-sik, also known as Chief Hong...",
+    calificacion: "4.8",
+    clasificacion: "13+",
+},
+     {
+    imagen: "https://assets-metrostyle.abs-cbn.com/prod/metro.style/media/2021/may/kdrama-hometown-cha-cha-cha-netflix-tvn-desktop.jpg?ext=.jpg",
+    titulo: "Glow up",
+    descripcion: "Yoon Hye-jin, an accomplished dentist from Seoul, goes to the idyllic seaside village of Gongjin on her late mother's birthday after her life goes awry. She spontaneously decides to open up a dental clinic, and gets to know jack-of-all-trades Hong Du-sik, also known as Chief Hong...",
+    calificacion: "4.8",
+    clasificacion: "13+",
+}
+];
+
+const tendencias = [
+    {imagen: "https://assets-metrostyle.abs-cbn.com/prod/metro.style/media/2021/may/kdrama-hometown-cha-cha-cha-netflix-tvn-desktop.jpg?ext=.jpg",
+    titulo: "Pinocho",
+    descripcion: "Yoon Hye-jin, an accomplished dentist from Seoul, goes to the idyllic seaside village of Gongjin on her late mother's birthday after her life goes awry. She spontaneously decides to open up a dental clinic, and gets to know jack-of-all-trades Hong Du-sik, also known as Chief Hong...",
+    calificacion: "4.8",
+    clasificacion: "13+",
+},
+     {
+    imagen: "https://assets-metrostyle.abs-cbn.com/prod/metro.style/media/2021/may/kdrama-hometown-cha-cha-cha-netflix-tvn-desktop.jpg?ext=.jpg",
+    titulo: "Brooklyn 99",
+    descripcion: "Yoon Hye-jin, an accomplished dentist from Seoul, goes to the idyllic seaside village of Gongjin on her late mother's birthday after her life goes awry. She spontaneously decides to open up a dental clinic, and gets to know jack-of-all-trades Hong Du-sik, also known as Chief Hong...",
+    calificacion: "4.8",
+    clasificacion: "13+",
+},
+     {
+    imagen: "https://assets-metrostyle.abs-cbn.com/prod/metro.style/media/2021/may/kdrama-hometown-cha-cha-cha-netflix-tvn-desktop.jpg?ext=.jpg",
+    titulo: "Bridgerton",
+    descripcion: "Yoon Hye-jin, an accomplished dentist from Seoul, goes to the idyllic seaside village of Gongjin on her late mother's birthday after her life goes awry. She spontaneously decides to open up a dental clinic, and gets to know jack-of-all-trades Hong Du-sik, also known as Chief Hong...",
+    calificacion: "4.8",
+    clasificacion: "13+",
+},
+     {
+    imagen: "https://assets-metrostyle.abs-cbn.com/prod/metro.style/media/2021/may/kdrama-hometown-cha-cha-cha-netflix-tvn-desktop.jpg?ext=.jpg",
+    titulo: "One day at the time",
+    descripcion: "Yoon Hye-jin, an accomplished dentist from Seoul, goes to the idyllic seaside village of Gongjin on her late mother's birthday after her life goes awry. She spontaneously decides to open up a dental clinic, and gets to know jack-of-all-trades Hong Du-sik, also known as Chief Hong...",
+    calificacion: "4.8",
+    clasificacion: "13+",
+},
+     {
+    imagen: "https://assets-metrostyle.abs-cbn.com/prod/metro.style/media/2021/may/kdrama-hometown-cha-cha-cha-netflix-tvn-desktop.jpg?ext=.jpg",
+    titulo: "Atypical",
+    descripcion: "Yoon Hye-jin, an accomplished dentist from Seoul, goes to the idyllic seaside village of Gongjin on her late mother's birthday after her life goes awry. She spontaneously decides to open up a dental clinic, and gets to know jack-of-all-trades Hong Du-sik, also known as Chief Hong...",
+    calificacion: "4.8",
+    clasificacion: "13+",
+}
+];
+
+const listaRecomendados = document.querySelector("#recomendados");
+
+recomendaciones.forEach(movie => {
+console.log(movie);
+
+    const imagenContenedor = document.createElement('div');
+    imagenContenedor.classList.add("imagen");
+
+    const imagen = document.createElement('img');
+    imagen.src = movie.imagen
+  
+    imagenContenedor.appendChild(imagen);
+    
+    listaRecomendados.appendChild(imagenContenedor);
+  });
